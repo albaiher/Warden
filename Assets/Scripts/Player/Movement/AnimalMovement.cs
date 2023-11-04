ff0d1359e815fd4f51ce53e9ea8692baeeee83cd
neku10 <cubedo.nieves@gmail.com>
2023-11-04 19:51:22 +0100

    Basic motion animations boar, deer and rabbit

--- Assets/Scripts/Player/Movement/AnimalMovement.cs
+++ Assets/Scripts/Player/Movement/AnimalMovement.cs
@@ -7,12 +7,19 @@ public class AnimalMovement : MonoBehaviour
 
     [SerializeField] List<GameObject> animalForms;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float RunSpeed = 12f;
+    private float actualSpeed = 5f;
+
     [SerializeField] private float turnSpeed = 100f;
     private GameObject currentAnimal;
     private float horizontalInput;
     private float verticalInput;
     
     private CharacterController characterController;
+
+    private float doubleTapTime = 0.1f; // Tiempo en segundos para considerar un doble toque
+    private float lastVerticalInputTime = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,10 +32,28 @@ public class AnimalMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        
+
+        if ( Input.GetButtonDown("Vertical") && (Time.time - lastVerticalInputTime <= doubleTapTime))
+        {
+            actualSpeed = RunSpeed;
+        }
+        else if (verticalInput <= 0)
+        {
+            actualSpeed = speed;
+        }
+
+        if (Input.GetButtonUp("Vertical") && verticalInput >= 0)
+        {
+            lastVerticalInputTime = Time.time;
+        }
+
+
         Vector3 move = characterController.transform.forward * verticalInput;
-        characterController.Move(move * Time.deltaTime * speed);
+        characterController.Move(move * Time.deltaTime * actualSpeed);
         characterController.transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
 
-        
     }
+
+
 }
